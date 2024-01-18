d72acedc3e6a331ec3ac726ac3d50a6a1e7f995c
atiribeja04 <atiribeja04@gmail.com>
2024-01-18 19:11:24 +0100

    test

---
+++ src/projecte2/botiga/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projecte2.botiga;
+
+/**
+ *
+ * @author batoi
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
